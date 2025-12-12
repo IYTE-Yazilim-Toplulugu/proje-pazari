6bfb47d92ccd283c5d4092c3c845bc403296d093
neonid0 <yunusbozcan@std.iyte.edu.tr>
2025-12-12 14:49:24 +0300

    feat: initialize next project

---
+++ app/projects/page.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/shared/button";
+import Link from "next/link";
+
+
+export default function ProjectsPage() {
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+
+            <Button />
+
+        </div>
+    )
+}
---
+++ components/shared/button.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function Button() {
+
+    return (
+        <Link href={"/"}>
+            Project Page
+        </Link>
+    )
+}

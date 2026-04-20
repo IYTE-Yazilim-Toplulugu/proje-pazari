3877cb7df44b21937a101a5325371f2b0c134ddd
UlasGokkaya <ulasgokkaya84@gmail.com>
2026-04-20 22:10:23 +0300

    fix: Old Folder 'app/projects' has been deleted.

--- app/projects/[project_id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ProjectDetailClient from './ProjectDetailClient';
-
-export default async function ProjectDetailPage({ params }: { params: Promise<{ project_id: string }> }) {
-  const { project_id } = await params;
-  return <ProjectDetailClient projectId={project_id} />;
-}
